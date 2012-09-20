fd2d099d4de04b0aac387dd66faa934ece30db83
Paul Bakker <pbakker@servoy.com>
2012-09-20 12:13:06 +0000

    Extended testsuite with Geo, Line & Pie chart

--- modDataVisualization_test/forms/test.js
+++ modDataVisualization_test/forms/test.js
@@ -35,7 +35,7 @@ function update(event) {
 		var sw = new scopes.modGoogleMaps.LatLng((Math.random() * 180).toFixed(0)-90, (Math.random() * 360).toFixed(0)-180)
 		var ne = new scopes.modGoogleMaps.LatLng((Math.random() * 180).toFixed(0)-90, (Math.random() * 360).toFixed(0)-180)
 		var bounds = new scopes.modGoogleMaps.LatLngBounds(sw,ne)
-		maps[i].setZoom((Math.random() * 10).toFixed(0))
+		maps[i].setZoom(parseInt((Math.random() * 10).toFixed(0)))
 		maps[i].panToBounds(bounds)
 	}
 }
@@ -49,34 +49,31 @@ function update(event) {
  */
 function onLoad(event) {
 	//Instantiate Gauges
-	var option = {
+	gauges.push(new scopes.modJustGauge.JustGauge(elements.gauge1,{
 		value: 67, 
 	    min: 0,
 	    max: 100,
 	    title: "Visitors"
-	}
-	gauges.push(new scopes.modJustGauge.JustGauge(elements.gauge1,option))
+	}))
 		
-	option = {
+	gauges.push(new scopes.modJustGauge.JustGauge(elements.gauge2,{
 		title: 'Features',
 		value: (Math.random() * 100).toFixed(0)
-	}
-	gauges.push(new scopes.modJustGauge.JustGauge(elements.gauge2,option))
+	}))
 	
-	option = {
+	gauges.push(new scopes.modJustGauge.JustGauge(elements.gauge3,{
 		title: 'Tasks',
 		value: (Math.random() * 100).toFixed(0)
-	}
-	gauges.push(new scopes.modJustGauge.JustGauge(elements.gauge3,option))
+	}))
 	
-	option = {
+	gauges.push(new scopes.modJustGauge.JustGauge(elements.gauge4,{
 		title: 'Bugs',
 		value: (Math.random() * 100).toFixed(0)
-	}
-	gauges.push(new scopes.modJustGauge.JustGauge(elements.gauge4,option))
+	}))
 	
 	//Instantiate Google GeoChart
 	var data = [
+		['Country', 'weight'],
 		['NL', 1000],
 		['GB', 500],
 		['RO', 50],
@@ -84,20 +81,19 @@ function onLoad(event) {
 	]
 	
 	var options = {
-		 colorAxis: {colors: ['green','orange']}
+		 colorAxis: {colors: ['yellow','red']}
 	}
-	var geo = scopes.modGoogleCharts.GeoChart(elements.geochart, data, options)
+	scopes.modGoogleCharts.GeoChart(elements.geochart, data, options)
 	
 	//Instantiate GoogleMaps
-	var map1 = new scopes.modGoogleMaps.GoogleMap(elements.maps, {
+	var map = new scopes.modGoogleMaps.GoogleMap(elements.maps, {
 		zoom: 8,
 		center: new scopes.modGoogleMaps.LatLng(-34.397, 150.644),
 		mapTypeId: scopes.modGoogleMaps.MapTypeIds.HYBRID
 	})
-	//map.render()
-	maps.push(map1)
+	maps.push(map)
 	
-	var map = new scopes.modGoogleMaps.GoogleMap(elements.map2, {
+	map = new scopes.modGoogleMaps.GoogleMap(elements.map2, {
 		zoom: 2,
 		center: new scopes.modGoogleMaps.LatLng(10, 20),
 		mapTypeId: scopes.modGoogleMaps.MapTypeIds.TERRAIN,
@@ -124,7 +120,115 @@ function onLoad(event) {
 		title: 'Hello Joas',
 		map: map
 	});
-//	//map.render()
 	
-//	plugins.scheduler.addJob('test',new Date(Date.now()+10000),update,10000)
+	
+	var lineChart = new scopes.modFlotr2.LineChart(elements.flotr2$line)
+	lineChart.draw({
+				data: [[1, 7000], [2, 13000], [3, 11000], [4, 15500], [5, 17000], [6, 21500], [7, 15136], [8, 8764], [9, 7345], [10, 11874], [11, 9837] ],
+				lines: {
+					lineWidth: 0.5, show: true, fill: true, fillColor: ['#fcefe3', '#efbc93'], fillOpacity: 0.34
+				}, 
+				points: { show: true, fill: true, fillColor: '#e79a70', hitRadius: 7 }
+			}, {
+				colors: ['#e79a70'],
+				shadowSize: 0,
+				fontColor: '#e1e1e1',
+				fontSize: 20,
+				xaxis: {
+					noTicks: 9,
+					min: 1.5,
+					max: 10.5
+				},
+				yaxis: {
+					showLabels: true,
+					min: 0,
+					max: 25100,
+					margin: false
+				},
+				grid: {
+					outline: 's',
+					verticalLines: false,
+					hoverable: true,
+					clickable: true,
+					color: '#a7a7a7'
+
+				},
+				selection: {
+					mode: 'x'
+				},
+				mouse: {
+					track: true,
+					relative: true,
+					position: 'n',
+					
+					lineColor: '#e79a70',
+					fillOpacity: 0,
+					sensibility: 20
+				}
+			})
+	
+	var barChart = new scopes.modFlotr2.PieChart(elements.flotr2$pie)
+	var d1 = [[0, 4]],
+    d2 = [[0, 3]],
+    d3 = [[0, 1.03]],
+    d4 = [[0, 3.5]]
+	barChart.draw([
+    { data : d1, label : 'Comedy' },
+    { data : d2, label : 'Action' },
+    { data : d3, label : 'Romance',
+      pie : {
+        explode : 50
+      }
+    },
+    { data : d4, label : 'Drama' }
+  ], {
+	    HtmlText : false,
+	    grid : {
+	      outline: '',
+	      verticalLines : false,
+	      horizontalLines : false
+	    },
+	    xaxis : { showLabels : false },
+	    yaxis : { showLabels : false },
+	    pie : {
+	      show : true, 
+	      explode : 6
+	    },
+	    mouse : { track : true },
+	    legend : {
+	      position : 'se',
+	      backgroundColor : '#D2E8FF'
+	    }
+	  })
+	
+//This bit is clientside code for Flotr to allow zooming... how to integrate this?
+//	// Draw graph with default options, overwriting with passed options
+//	function drawGraph(opts) {
+//		// Clone the options, so the 'options' variable always keeps intact.
+//		o = Flotr._.extend(Flotr._.clone(options), opts || { });
+//
+//		// Return a new graph.
+//		return Flotr.draw(container,
+//			[d2],
+//			o
+//		);
+//	}
+//
+//	graph = drawGraph();
+//
+//	Flotr.EventAdapter.observe(container, 'flotr:select', function(area) {
+//			// Draw selected area
+//			graph = drawGraph({
+//				xaxis: { min: area.x1, max: area.x2, tickFormatter: monthValue},
+//				yaxis: { min: area.y1, max: area.y2, margin: false}
+//			});
+//		});
+//
+//	// When graph is clicked, draw the graph with default area.
+//	Flotr.EventAdapter.observe(container, 'flotr:click', function() {
+//			graph = drawGraph();
+//		});
+	
+	//Start automatic update
+	plugins.scheduler.addJob('test',new Date(Date.now()+10000),update,10000)
 }
