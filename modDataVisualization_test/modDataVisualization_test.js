a5bc997bb4b51503d3d8f5105cd67e57f4d26309
Paul Bakker <pbakker@servoy.com>
2012-12-05 20:59:27 +0000

    Added dummy unitTest methods in test solutions that didn't contain any UnitTest yet, in order to prevent the Build Server from failing due to missing UnitTests in the test solution

---
+++ modDataVisualization_test/modDataVisualization_test.js
@@ -0,0 +1,6 @@
+/**
+ * @properties={typeid:24,uuid:"637D9626-FCFA-4CA0-A4FE-14CB8F31FB25"}
+ */
+function testDummy() {
+	//dummy test function so build server doesn't fail the build due to the test solution nog having any UnitTests
+}
