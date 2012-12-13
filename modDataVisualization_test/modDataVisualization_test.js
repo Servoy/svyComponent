107aa0b26eb36c0f3e585ec2b19c25689ded1877
Paul Bakker <pbakker@servoy.com>
2012-12-13 21:22:03 +0000

    Added unit test Made test form the first form again

--- modDataVisualization_test/modDataVisualization_test.js
+++ modDataVisualization_test/modDataVisualization_test.js
@@ -1,6 +1,31 @@
 /**
- * @properties={typeid:24,uuid:"637D9626-FCFA-4CA0-A4FE-14CB8F31FB25"}
+ * @properties={typeid:24,uuid:"336166B1-D914-4701-B245-69C25DC2781B"}
  */
-function testDummy() {
-	//dummy test function so build server doesn't fail the build due to the test solution nog having any UnitTests
+function testAbstractMethodImpl4AbstractDataVisualization() {
+	//All instances of AbstractDataVisualization should implement getBrowserId(), but not getId()
+	var instances = scopes.svyUI.getJSFormInstances(solutionModel.getForm('AbstractDataVisualizer'))
+	
+	instances.forEach(function(element, index, array){
+		/** @type {JSForm} */
+		var jsForm = element
+		var methods = jsForm.getMethods(false)
+		var hasGetBrowserId = false
+		var hasGetId = false
+		methods.every(function(el, i, ar) {
+			/** @type {JSMethod} */
+			var method = el
+			switch (method.getName()) {
+				case 'getBrowserId':
+					hasGetBrowserId = true
+					break;
+				case 'getId':
+					hasGetId = true
+					break;
+				default:
+					break;
+			}
+		})
+		jsunit.assertTrue('AbstractDataVisualization instances MUST override .getBrowserId()', hasGetBrowserId)
+		jsunit.assertFalse('AbstractDataVisualization instances must NOT override .getId()', hasGetId)
+	})
 }
