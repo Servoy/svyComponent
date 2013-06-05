294f9185d92ebdc0c146260b392bab9469981f40
Paul Bakker <pbakker@servoy.com>
2013-06-05 13:34:28 +0000

    Removed empty globals.js

--- modDataVisualization_test/globals.js
+++
@@ -1,9 +0,0 @@
-
-/**
- * Callback method for when solution is opened.
- *
- * @properties={typeid:24,uuid:"79B5DC9E-3287-4850-8C63-320BFB83C92F"}
- */
-function onSolutionOpen() {
-	application.putClientProperty(APP_UI_PROPERTY.TABLEVIEW_WC_DEFAULT_SCROLLABLE, true);
-}
