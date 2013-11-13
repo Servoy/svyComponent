decb1f2180be5483792033c4285082d7b4bfb098
Paul Bakker <pbakker@servoy.com>
2013-11-13 15:41:04 +0000

    Added IComponentResourceProvider interface form

---
+++ modComponent/forms/IComponentResourceProvider.frm
@@ -0,0 +1,5 @@
+encapsulation:60,
+name:"IComponentResourceProvider",
+showInMenu:true,
+typeid:3,
+uuid:"84206651-55C8-4697-BB0E-1531AFDDAE78"
---
+++ modComponent/forms/IComponentResourceProvider.js
@@ -0,0 +1,7 @@
+/**
+ * @param {RuntimeForm<ComponentBase>} container the panel in which the visualization is displayed. Note: all existing tabs in the panel will be removed
+ *
+ * @properties={typeid:24,uuid:"D2FE3513-CE71-4C83-8625-86876A4F63BF"}
+ */
+function addDependancies(container) { 
+}
